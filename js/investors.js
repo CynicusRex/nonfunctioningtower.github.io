f6a0a874d2aca5c37ea36e45493d03224733ad14
CynicusRex <cynicusrex@vivaldi.net>
2022-03-07 13:12:32 +0100

    style update

--- js/investors.js
+++ js/investors.js
@@ -1,32 +1,33 @@
-let investorList = [
+let ledger = [
     {
-        "number": 1,
+        "transactionNumber": 2,
         "name": "Example",
-        "URL": "https://www.cynicusrex.com",
-        "paid": 175
+        "URL": "",
+        "paid": 0.9
     },
     {
-        "number": 3,
+        "transactionNumber": 1,
         "name": "Example",
-        "URL": "https://www.cynicusrex.com",
-        "paid": 107
-    },
-    {
-        "number": 100,
-        "name": "Example",
-        "URL": "https://www.cynicusrex.com",
-        "paid": 3.5
+        "URL": "",
+        "paid": 0.5
     }
 ];
 
-let investorListParagraph = "№ 0: <a href='https://physicstoday.scitation.org/doi/10.1063/PT.3.4521'>Isaac Newton</a> (€350). <br>";
-for (let i = 0; i < investorList.length; i++){
-    investorListParagraph += "№ " + investorList[i].number + ": " + 
-    "<a href='" + investorList[i].URL + "'>" + investorList[i].name + "</a>" + 
-    " (€" + investorList[i].paid.toFixed(2) + "), ";
+let ledgerParagraph = "4: <a href='https://physicstoday.scitation.org/doi/10.1063/PT.3.4521'>Isaac Newton</a> ($450), ";
+for (let i = 0; i < ledger.length; i++){
+    ledgerParagraph += ledger[i].transactionNumber + ": ";
 
-    if (i == investorList.length-1){
-        investorListParagraph = investorListParagraph.slice(0,-2) + ".";
+    if  (ledger[i].URL != "") {
+        ledgerParagraph += "<a href='" + ledger[i].URL + "'>" + ledger[i].name + "</a>";
+    }
+    else {
+        ledgerParagraph += ledger[i].name;
+    }
+
+    ledgerParagraph += " ($" + ledger[i].paid.toFixed(2) + "), "
+    
+    if (i == ledger.length-1){
+        ledgerParagraph = ledgerParagraph.slice(0,-2) + ".";
     };
 }
-document.getElementById("insertjshere").innerHTML = investorListParagraph;
+document.getElementById("insertjshere").innerHTML = ledgerParagraph;
