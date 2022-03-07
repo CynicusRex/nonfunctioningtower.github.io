8d3ddd8866923de84e56c81dfd3b92fa1408c25e
CynicusRex <cynicusrex@vivaldi.net>
2022-03-07 18:13:42 +0100

    adds date to transaction

--- js/investors.js
+++ js/investors.js
@@ -1,27 +1,23 @@
 let ledger = [
     {
-        "transactionNumber": 2,
-        "name": "Example",
-        "URL": "",
-        "paid": 1.5
-    },
-    {
         "transactionNumber": 1,
-        "name": "Example",
-        "URL": "",
-        "paid": 0.5
+        "name": "Infernuz",
+        "URL": "https://www.cisa.gov/sites/default/files/publications/Cybersecurity%20Awareness%20Month%202021%20-%20Phishing%20Tip%20Sheet.pdf",
+        "paid": "€2.00",
+        "date": "7 March 2022"
     },
     {
         "transactionNumber": 0,
         "name": "Isaac Newton",
         "URL": "https://physicstoday.scitation.org/doi/10.1063/PT.3.4521",
-        "paid": 27
+        "paid": "∅",
+        "date": ""
     }
 ];
 
 let ledgerParagraph = "";
 for (let i = 0; i < ledger.length; i++){
-    ledgerParagraph += ledger[i].transactionNumber + ": ";
+    ledgerParagraph += ledger[i].transactionNumber + ": <span title='" + ledger[i].date + "'>";
 
     if  (ledger[i].URL != "") {
         ledgerParagraph += "<a href='" + ledger[i].URL + "'>" + ledger[i].name + "</a>";
@@ -30,7 +26,7 @@ for (let i = 0; i < ledger.length; i++){
         ledgerParagraph += ledger[i].name;
     }
 
-    ledgerParagraph += " ($" + ledger[i].paid.toFixed(2) + "), "
+    ledgerParagraph += " (" + ledger[i].paid + ")</span>, "
     
     if (i == ledger.length-1){
         ledgerParagraph = ledgerParagraph.slice(0,-2) + ".";
